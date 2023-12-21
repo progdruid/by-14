cc2d8790d660456f36718a5374bc66af26afb7ac
progdruid <progdruid@gmail.com>
2023-12-21 15:54:04 +0000

    project creation and basic setup

---
+++ Source/hooker/hookerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "hookerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOOKER_API AhookerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
