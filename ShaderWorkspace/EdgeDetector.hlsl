204e59b311fe2d6da125c81d242a2deaa36e8473
progdruid <progdruid@gmail.com>
2024-01-12 23:24:01 +0000

    both depth and normals are used for edge detection now

--- ShaderWorkspace/EdgeDetector.hlsl
+++ ShaderWorkspace/EdgeDetector.hlsl
@@ -3,16 +3,28 @@
 
 float centerDepth = SceneTextureLookup(ViewportUVToSceneTextureUV(UV, 1), 1, false).r;
 float depthScaler = centerDepth / DefaultDepth;
-float scaledEdgeOffset = EdgeUVOffset / depthScaler;
-float scaledThreshold = EdgeThreshold * depthScaler;
+float2 scaledEdgeOffset = EdgeUVOffset / depthScaler;
+if (scaledEdgeOffset.x < MinUVOffset)
+    scaledEdgeOffset.x = MinUVOffset;
+if (scaledEdgeOffset.y < MinUVOffset)
+    scaledEdgeOffset.y = MinUVOffset;
+float scaledDepthThreshold = DepthThreshold * depthScaler;
 
 double curv = 0;
 float angleStep = PI / NumberEdgeChecks;
 for (int i = 0; i < NumberEdgeChecks; i++)
 {
-    double2 offsetVec = double2(cos(angleStep * i), sin(angleStep * i)) * scaledEdgeOffset;
-    double firstDepth = SceneTextureLookup(ViewportUVToSceneTextureUV(UV+offsetVec, 1), 1, false).r;
-    double secondDepth = SceneTextureLookup(ViewportUVToSceneTextureUV(UV-offsetVec, 1), 1, false).r;
-    curv = max(curv, abs(firstDepth + secondDepth - centerDepth * 2));
+    float2 offsetVec = float2(cos(angleStep * i) * scaledEdgeOffset.x, sin(angleStep * i) * scaledEdgeOffset.y);
+
+    float firstDepth = SceneTextureLookup(ViewportUVToSceneTextureUV(UV + offsetVec, 1), 1, false).r;
+    float secondDepth = SceneTextureLookup(ViewportUVToSceneTextureUV(UV - offsetVec, 1), 1, false).r;
+    bool depthDetected = abs(firstDepth + secondDepth - centerDepth * 2) > scaledDepthThreshold * 2;
+
+    float3 firstNormal = SceneTextureLookup(ViewportUVToSceneTextureUV(UV + offsetVec, 8), 8, false);
+    float3 secondNormal = SceneTextureLookup(ViewportUVToSceneTextureUV(UV - offsetVec, 8), 8, false);
+    bool normalDetected = dot(firstNormal, secondNormal) < NormalThreshold;
+
+    if (depthDetected || normalDetected)
+        return 1;
 }
-return curv > scaledThreshold * 2;
+return 0;
