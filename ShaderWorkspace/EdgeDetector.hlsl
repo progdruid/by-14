d6e13dbaebea20ad541761a750fbe55b8c367458
progdruid <progdruid@gmail.com>
2024-06-17 00:13:01 +0100

    bugfix: screen edge artifacts

--- ShaderWorkspace/EdgeDetector.hlsl
+++ ShaderWorkspace/EdgeDetector.hlsl
@@ -12,13 +12,22 @@ float angleStep = PI / NumberEdgeChecks;
 for (int i = 0; i < NumberEdgeChecks; i++)
 {
     float2 offsetVec = float2(cos(angleStep * i) * scaledEdgeOffset.x, sin(angleStep * i) * scaledEdgeOffset.y);
+    float2 firstVec = UV - offsetVec;
+    //firstVec.x = clamp(firstVec.x, 0.f, 1.f);
+    //firstVec.y = clamp(firstVec.y, 0.f, 1.f);
+    float2 secondVec = UV + offsetVec;
+    //secondVec.y = clamp(secondVec.x, 0.f, 1.f);
+    //secondVec.y = clamp(secondVec.y, 0.f, 1.f);
+    if ((firstVec.x < 0 || secondVec.x < 0 || firstVec.x > 1 || secondVec.x > 1)
+        || (firstVec.y < 0 || secondVec.y < 0 || firstVec.y > 1 || secondVec.y > 1))    
+        continue;
 
-    float firstDepth = SceneTextureLookup(ViewportUVToSceneTextureUV(UV + offsetVec, 1), 1, false).r;
-    float secondDepth = SceneTextureLookup(ViewportUVToSceneTextureUV(UV - offsetVec, 1), 1, false).r;
+    float firstDepth = SceneTextureLookup(ViewportUVToSceneTextureUV(firstVec, 1), 1, false).r;
+    float secondDepth = SceneTextureLookup(ViewportUVToSceneTextureUV(secondVec, 1), 1, false).r;
     bool depthDetected = abs(firstDepth + secondDepth - centerDepth * 2) > DepthThreshold * 2;
 
-    float3 firstNormal = SceneTextureLookup(ViewportUVToSceneTextureUV(UV + offsetVec, 8), 8, false);
-    float3 secondNormal = SceneTextureLookup(ViewportUVToSceneTextureUV(UV - offsetVec, 8), 8, false);
+    float3 firstNormal = SceneTextureLookup(ViewportUVToSceneTextureUV(firstVec, 8), 8, false);
+    float3 secondNormal = SceneTextureLookup(ViewportUVToSceneTextureUV(secondVec, 8), 8, false);
     bool normalDetected = dot(firstNormal, secondNormal) < (1. - NormalThreshold);
     
     if (depthDetected || normalDetected)
